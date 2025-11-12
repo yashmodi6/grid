7a14eb1bd06bce1b9d74cda8da06b1b1c8e8d3bf
Yash Modi <yovotheheroes@gmail.com>
2025-11-12 23:27:29 +0530

    ⚙️ chore: init Next.js + TypeScript + Tailwind + shadcn setup

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 100,
+  bracketSameLine: true,
+  bracketSpacing: true,
+  arrowParens: true,
+};
+
+export default config;
